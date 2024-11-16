40eb80a9d236e60d09a89e1edadbae61ce31be50
Yash <yashwanth.venkatesan@gmail.com>
2024-11-16 15:53:11 -0500

    changes to colorpicker

--- src/components/menu/ChangeColors.tsx
+++ src/components/menu/ChangeColors.tsx
@@ -1,11 +1,9 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import MenuHeaders from "./MenuHeaders";
 import { useThemeStore } from "../../stores/ThemeStore";
 import { HexColor } from "../../lib/types";
 import { ColorPicker, useColor } from "react-color-palette";
 import "react-color-palette/css";
-
-
 
 const colorConfig = {
   background: { display: "Background", themeKey: "bg" },
@@ -27,7 +25,7 @@ const ChangeColors = () => {
         isOpen={isOpen}
       />
       {isOpen && (
-        <div className="grid grid-cols-1 gap-4">
+        <div className="grid grid-cols-2 gap-4">
           {Object.values(colorConfig).map(({ display }) => (
             <ColorInput key={display} title={display} />
           ))}
@@ -41,20 +39,37 @@ const ColorInput = ({ title }: { title: string }) => {
   const { updateTheme, ...theme } = useThemeStore();
   const config = Object.values(colorConfig).find((c) => c.display === title)!;
   const [color, setColor] = useColor(theme[config.themeKey] as HexColor);
-  // ! is used to ensure that the config is not undefined
-  // config is used to set the value of the specific key in the colorConfig object
+  const [isOpen, setIsOpen] = useState(false);
+  const pickerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (pickerRef.current && !pickerRef.current.contains(event.target as Node)) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
 
   return (
-    <div className="flex flex-col ">
+    <>
       <h3>{title}</h3>
-      <ColorPicker 
-        color={color}
-        onChange={(e ) => {
-          updateTheme({ [config.themeKey]: e.hex });
-          setColor(e);
-        }}
-      />
-    </div>
+      <div className="w-20 h-8 border-2 border-black rounded-lg shadow-lg relative" style={{ backgroundColor: theme[config.themeKey] }} onClick={() => setIsOpen(!isOpen)}>
+        {isOpen && <div ref={pickerRef} className="absolute -top-60 right-8" onClick={(e) => e.stopPropagation()}>
+          <ColorPicker 
+            color={color}
+            onChange={(e) => {
+              updateTheme({ [config.themeKey]: e.hex });
+              setColor(e);
+            }}
+          />
+        </div>}
+      </div>
+    </>
   );
 };
 
